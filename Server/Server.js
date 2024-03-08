f4027852effc380dd7a01b466459c67b2af88ccf
victor <mgbemenaosonduv@gmail.com>
2024-03-08 09:43:56 +0100

    MODIFICATION:     integerate MongoDB to store user new created user

--- Server/Server.js
+++ Server/Server.js
@@ -93,8 +93,7 @@ const server = http.createServer((req, res) => {
     userInfo += chunk.toString();
   });
 
-  req.on("end", () => {
-    // Set CORS headers
+  req.on("end", async () => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader(
@@ -104,136 +103,129 @@ const server = http.createServer((req, res) => {
 
     switch (req.url) {
       case "/Register":
-        //REGISTERATION ENDPOINT ------------------------------------------------------------------------------------------------------------------------------------------------
         if (req.method === "POST") {
-          // Parse the received JSON data
           const userData = JSON.parse(userInfo);
-
-          // Create user using CreateUser function
           const newUser = CreateUser(userData);
 
-          //since DB is not available for now. i will use a mocked DB
-          const newDbData = mock_DB.push(newUser);
-          //TODO: Remember to uncomment model
-          //Model.create(newUser);
-          res.writeHead(201, { "Content-Type": "application/json" });
-          res.end(
-            JSON.stringify({
-              message: "User registered successfully",
-              user: newUser,
-              DBSize: newDbData,
-            })
-          );
+          try {
+            const savedUser = await new UserModel(newUser).save();
+            res.writeHead(201, { "Content-Type": "application/json" });
+            res.end(
+              JSON.stringify({
+                message: "User registered successfully",
+                user: savedUser,
+              })
+            );
+          } catch (error) {
+            res.writeHead(500, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ error: "Internal server error" }));
+          }
         }
         break;
 
       case "/Parcels":
-        // POST NEW PARCEL ----------------------------------------------------------------------------------------------------------------------------------------------------
         if (req.method === "POST") {
-          // Retrieve data parameters for updating parcel
           const { userID, destination, sender, receiver, coordinates } =
             JSON.parse(userInfo);
 
-          // Find the user in the mock database
-          const foundUserIndex = mock_DB.findIndex(
-            (user) => user.ID === userID
-          );
+          try {
+            const foundUser = await UserModel.findOne({ ID: userID });
 
-          if (foundUserIndex !== -1) {
-            // User found in the database
-            const newParcel = {
-              parcelLocation: destination,
-              Sender: sender,
-              Reciever: receiver,
-              TrackingNumber: IdGen(15),
-              coordinates: { Lat: coordinates.lat, lon: coordinates.lon },
-            };
+            if (foundUser) {
+              const newParcel = {
+                parcelLocation: destination,
+                Sender: sender,
+                Reciever: receiver,
+                TrackingNumber: IdGen(15),
+                coordinates: { Lat: coordinates.lat, lon: coordinates.lon },
+              };
 
-            // Add the new parcel to the user's PARCEL array
-            mock_DB[foundUserIndex].PARCEL.push(newParcel);
+              foundUser.PARCEL.push(newParcel);
+              const savedUser = await foundUser.save();
 
-            // Respond with success message and updated user data
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(
-              JSON.stringify({
-                message: "Parcel added to user successfully",
-                user: mock_DB[foundUserIndex],
-              })
-            );
-          } else {
-            // User not found in the database
-            res.writeHead(404, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ error: "User not found" }));
-          }
-        }
-        break;
-
-      case "/updateCoordinates":
-        // updates parcel coordinates ENDPOINT---------------------------------------------------------------------------------------------------------------------------------
-        if (req.method === "PUT") {
-          const { userId, parcelId, coordinates } = JSON.parse(userInfo);
-          const user = mock_DB.find((user) => user.ID === userId);
-          if (user) console.log(user);
-          if (user) {
-            const parcelIndex = user.PARCEL.findIndex(
-              (p) => p.TrackingNumber === parcelId
-            );
-
-            const newCoordinates = {
-              lat: coordinates.lat,
-              lon: coordinates.lon,
-            };
-
-            if (parcelIndex === -1) {
-              res.writeHead(404, { "Content-Type": "application/json" });
-              res.end(JSON.stringify({ error: "Parcel not found" }));
-            } else {
-              user.PARCEL[parcelIndex].coordinates = newCoordinates;
               res.writeHead(200, { "Content-Type": "application/json" });
               res.end(
                 JSON.stringify({
-                  message: "Parcel coordinates updated successfully",
-                  user: user,
+                  message: "Parcel added to user successfully",
+                  user: savedUser,
                 })
               );
+            } else {
+              res.writeHead(404, { "Content-Type": "application/json" });
+              res.end(JSON.stringify({ error: "User not found" }));
             }
-          } else {
-            res.writeHead(404, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ error: "User not found" }));
+          } catch (error) {
+            res.writeHead(500, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ error: "Internal server error" }));
           }
         }
         break;
+
+
+      case "/updateCoordinates":
+        if (req.method === "PUT") {
+          const { userId, parcelId, coordinates } = JSON.parse(userInfo);
+
+          try {
+            const user = await UserModel.findOne({ ID: userId });
+
+            if (user) {
+              const parcelIndex = user.PARCEL.findIndex(
+                (p) => p.TrackingNumber === parcelId
+              );
+
+              if (parcelIndex === -1) {
+                res.writeHead(404, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ error: "Parcel not found" }));
+              } else {
+                user.PARCEL[parcelIndex].coordinates = {
+                  lat: coordinates.lat,
+                  lon: coordinates.lon,
+                };
+
+                const savedUser = await user.save();
+
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.end(
+                  JSON.stringify({
+                    message: "Parcel coordinates updated successfully",
+                    user: savedUser,
+                  })
+                );
+              }
+            } else {
+              res.writeHead(404, { "Content-Type": "application/json" });
+              res.end(JSON.stringify({ error: "User not found" }));
+            }
+          } catch (error) {
+            res.writeHead(500, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ error: "Internal server error" }));
+          }
+        }
+        break;
+
+
       case "/users":
         if (req.method === "GET") {
-          // Query the database for all registered users
-          let Users = mock_DB.map((user) => {
-            return user;
-          });
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(Users));
-
-          //TODO: Remember to uncomment
-          // Model.find({}, (err, users) => {
-          //   if (err) {
-          //     res.writeHead(500, { "Content-Type": "application/json" });
-          //     res.end(JSON.stringify({ error: "Internal server error" }));
-          //   } else {
-          //     // Send the list of registered users as a response
-          //     res.writeHead(200, { "Content-Type": "application/json" });
-          //     res.end(JSON.stringify(users));
-          //   }
-          // });
+          try {
+            const users = await UserModel.find({});
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(users));
+          } catch (error) {
+            res.writeHead(500, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ error: "Internal server error" }));
+          }
         }
         break;
 
       default:
-        // If the route is not found or the method is not allowed
         res.writeHead(404, { "Content-Type": "text/plain" });
         res.end("Not Found");
         break;
     }
   });
 });
+
 
 // Set the port to listen on
 const PORT = process.env.PORT || 4500;
