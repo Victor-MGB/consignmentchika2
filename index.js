6b42de1cde10953dc99df086928b5593756a0604
victor <mgbemenaosonduv@gmail.com>
2024-03-09 18:11:51 +0100

    cors to every website added

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
