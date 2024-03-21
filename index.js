110aea6a8dcd7bc2b1febacddd9a9938569cefe9
victor <mgbemenaosonduv@gmail.com>
2024-03-21 07:11:20 +0100

    modified the server host

--- index.js
+++ index.js
@@ -353,7 +353,7 @@ app.use((req, res) => {
   res.status(404).send("Not Found");
 });
 
-const PORT = process.env.PORT || 4500;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
